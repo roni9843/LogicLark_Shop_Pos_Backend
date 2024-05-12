86d17ff7a5fc79824b968dddc4e6ac74acbe282c
roni9843 <ronironi98439843@gmail.com>
2024-05-12 20:32:13 +0600

    Your commit message here

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const config = {
   PORT: process.env.PORT || 8000,
-  DB_CONN: `mongodb+srv://organicUser:${process.env.DB_CONN}@cluster0.tibcl.mongodb.net/HasanShop`,
+  DB_CONN: `mongodb+srv://roni9843:${process.env.DB_CONN}@cluster0.utd8f8o.mongodb.net/HasanShop`,
 };
 
 module.exports = config;
