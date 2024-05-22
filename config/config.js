a72c672a86f629deb8e3f2b6719ee2635a444ecb
Jubayth Hossen Roni <150267619+roni9843@users.noreply.github.com>
2024-05-22 22:51:45 +0600

    Update config.js

--- config/config.js
+++ config/config.js
@@ -2,7 +2,8 @@ require("dotenv").config();
 
 const config = {
   PORT: process.env.PORT || 8000,
-  DB_CONN: `mongodb+srv://roni9843:${process.env.DB_CONN}@cluster0.utd8f8o.mongodb.net/HasanShop`,
+//  DB_CONN: `mongodb+srv://roni9843:${process.env.DB_CONN}@cluster0.utd8f8o.mongodb.net/HasanShop`,
+  DB_CONN: `mongodb+srv://roni9843:roni9843@cluster0.utd8f8o.mongodb.net/HasanShop`,
 };
 
 module.exports = config;
