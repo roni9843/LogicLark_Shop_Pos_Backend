d2a10e95c948a83ab079dde6e5c4515d018c564c
roni9843 <ronironi98439843@gmail.com>
2024-06-24 17:06:14 +0600

    add config

--- config/config.js
+++ config/config.js
@@ -2,9 +2,9 @@ require("dotenv").config();
 
 const config = {
   PORT: process.env.PORT || 8000,
-  DB_CONN: `mongodb+srv://organicUser:${process.env.DB_CONN}@cluster0.tibcl.mongodb.net/HasanShop`, // ==> text
+  // DB_CONN: `mongodb+srv://organicUser:${process.env.DB_CONN}@cluster0.tibcl.mongodb.net/HasanShop`, // ==> text
   //DB_CONN: `mongodb+srv://roni9843:${process.env.DB_CONN}@cluster0.utd8f8o.mongodb.net/HasanShop`, // ==> text
-  // DB_CONN: `mongodb+srv://roni9843:roni9843@cluster0.utd8f8o.mongodb.net/HasanShop`, // Prodction
+  DB_CONN: `mongodb+srv://roni9843:roni9843@cluster0.utd8f8o.mongodb.net/HasanShop`, // Prodction
 };
 
 module.exports = config;
